d92c88ca6b6f5cf9ffec06aad728e8a78ae34f67
abdousalam <salamniass@gmail.com>
2019-12-16 18:54:01 +0100

    Completed the reveal on behavior

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,58 @@
+import throttle from "lodash/throttle";
+import debounce from "lodash/debounce";
+class RevealOnScroll {
+  constructor(els, percentagScroll) {
+    this.percentagScroll = percentagScroll;
+    this.itemsToReveal = els;
+    this.browserHeight = window.innerHeight;
+    this.hideInitially();
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
+    this.events();
+  }
+
+  events() {
+    window.addEventListener("scroll", this.scrollThrottle);
+    window.addEventListener(
+      "resize",
+      debounce(() => {
+        console.log("resize just ran");
+        this.browserHeight = window.innerHeight;
+      }, 333)
+    );
+  }
+
+  calcCaller() {
+    console.log("Scroll function ran");
+    this.itemsToReveal.forEach(el => {
+      if (el.isRevealed == false) {
+        this.calculateIfScrolledTO(el);
+      }
+    });
+  }
+
+  hideInitially() {
+    this.itemsToReveal.forEach(el => {
+      el.classList.add("reveal-item");
+      el.isRevealed = false;
+    });
+    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
+  }
+
+  calculateIfScrolledTO(el) {
+    if (window.scrollY + this.browserHeight > el.offsetTop) {
+      console.log("Element was calculated");
+
+      let scrollPercent =
+        (el.getBoundingClientRect().top / this.browserHeight) * 100;
+      if (scrollPercent < this.percentagScroll) {
+        el.classList.add("reveal-item--is-visible");
+        el.isRevealed = true;
+        if (el.isLastItem) {
+          window.removeEventListener("scroll", this.scrollThrottle);
+        }
+      }
+    }
+  }
+}
+
+export default RevealOnScroll;
