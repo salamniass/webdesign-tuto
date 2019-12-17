2517d52e28120cda9a5d7e4e3a2da4ec7ae9174b
abdousalam <salamniass@gmail.com>
2019-12-17 17:19:29 +0100

    completed our biuld

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,7 +3,6 @@ import "lazysizes";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
-
 let mobileMenu = new MobileMenu();
 
 let stickyHeader = new StickyHeader();
